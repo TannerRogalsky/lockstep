32c6455819a2d5bd5c787774eb1eeda798b244f0
Tanner Rogalsky <tanner@tannerrogalsky.com>
2020-09-16 07:26:21 -0400

    can i just use a webpack plugin to create the wasm package?

--- www/webpack.prod.js
+++ www/webpack.prod.js
@@ -2,14 +2,18 @@ const path = require('path');
 const { merge } = require('webpack-merge');
 // const { BundleAnalyzerPlugin } = require('webpack-bundle-analyzer');
 const common = require('./webpack.common.js');
+const WasmPackPlugin = require("@wasm-tool/wasm-pack-plugin");
 
 module.exports = merge(common, {
-  output: {
-    path: path.resolve(__dirname, '..', 'server', 'public'),
-    filename: 'bootstrap.js',
-  },
-  mode: 'production',
-  plugins: [
-    // new BundleAnalyzerPlugin()
-  ],
+    output: {
+        path: path.resolve(__dirname, '..', 'server', 'public'),
+        filename: 'bootstrap.js',
+    },
+    mode: 'production',
+    plugins: [
+        new WasmPackPlugin({
+            crateDirectory: path.resolve(__dirname, '..', 'client'),
+            outDir: '../pkg',
+        }),
+    ],
 });
