c8c68d66cd5bbab7799905332af47661c5e83d43
Tanner Rogalsky <tanner@tannerrogalsky.com>
2020-09-21 08:42:43 -0400

    position and velocities integrated based on masses

--- nbody/src/lib.rs
+++ nbody/src/lib.rs
@@ -74,6 +74,10 @@ fn lerp_point(v0: Point2D, v1: Point2D, t: Float) -> Point2D {
     Point2D::new(lerp(v0.x, v1.x, t), lerp(v0.y, v1.y, t))
 }
 
+fn lerp_vector(v0: Vector2D, v1: Vector2D, t: Float) -> Vector2D {
+    Vector2D::new(lerp(v0.x, v1.x, t), lerp(v0.y, v1.y, t))
+}
+
 // TODO: Need a cbrt implementation for fixed point floats
 fn cbrt(v: Float) -> Float {
     let v: f32 = v.to_num();
@@ -172,10 +176,20 @@ impl Simulation {
             if let Some(body2_index) = self.bodies.iter().position(|body| body.id == id2) {
                 let body2 = self.bodies.swap_remove(body2_index);
                 if let Some(body1) = self.bodies.iter_mut().find(|body| body.id == id1) {
+                    let (v0, v1, t) = if body1.mass > body2.mass {
+                        (body1.position, body2.position, body2.mass / body1.mass)
+                    } else {
+                        (body2.position, body1.position, body1.mass / body2.mass)
+                    };
+                    let t = t / Float::from_num(2);
+                    body1.position = lerp_point(v0, v1, t);
+                    let (v0, v1) = if body1.mass > body2.mass {
+                        (body1.velocity, body2.velocity)
+                    } else {
+                        (body2.velocity, body1.velocity)
+                    };
+                    body1.velocity = lerp_vector(v0, v1, t);
                     body1.mass += body2.mass;
-                    body1.position =
-                        lerp_point(body1.position, body2.position, Float::from_num(0.5));
-                    body1.velocity = Vector2D::new(Float::from_bits(0), Float::from_bits(0));
                 } else {
                     log::error!("Found only one side of a collision.")
                 }
