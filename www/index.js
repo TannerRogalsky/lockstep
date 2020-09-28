c536cb2e0579f595d3fba1fe5c6503a3bcd5b9d1
Tanner Rogalsky <tanner@tannerrogalsky.com>
2020-09-28 08:56:33 -0400

    overlay

--- www/index.js
+++ www/index.js
@@ -18,49 +18,53 @@ async function run() {
   const state = State.from_raw(state_buffer, channel);
 
   const canvas = document.getElementById('canvas');
-  const bg_canvas = document.getElementById('bg');
+  const overlay = document.getElementById('overlay');
   const ctx = canvas.getContext('2d');
-  const bg_ctx = canvas.getContext('2d');
+  const overlay_ctx = overlay.getContext('2d');
 
   const resize = (canvas) => {
     canvas.width = document.body.clientWidth;
     canvas.height = document.body.clientHeight;
   };
   canvas.addEventListener('resize', resize.bind(null, canvas));
+  overlay.addEventListener('resize', resize.bind(null, overlay));
   resize(canvas);
+  resize(overlay);
+
+  overlay.addEventListener('contextmenu', (event) => {
+    event.preventDefault();
+  });
 
   let mouseDown = null;
-  canvas.addEventListener('mousedown', (event) => {
-    mouseDown = [event.x, event.y];
+  overlay.addEventListener('mousedown', (event) => {
+    if (event.button == 0) {
+      mouseDown = [event.x, event.y];
+    }
   });
 
   let mousePos = [0, 0];
-  canvas.addEventListener('mousemove', (event) => {
+  overlay.addEventListener('mousemove', (event) => {
     mousePos[0] = event.x;
     mousePos[1] = event.y;
   });
 
   let massOptions = document.getElementById("masses");
-  canvas.addEventListener('mouseup', (event) => {
+  overlay.addEventListener('mouseup', (event) => {
     if (!mouseDown) {
       return;
     }
 
-    let mass = parseInt(massOptions.selectedOptions[0].value);
-    state.mouse_click_event(mouseDown[0], mouseDown[1], mass, event.x, event.y);
-    mouseDown = null;
+    if (event.button == 0) {
+      let mass = parseInt(massOptions.selectedOptions[0].value);
+      state.mouse_click_event(mouseDown[0], mouseDown[1], mass, event.x, event.y);
+      mouseDown = null;
+    }
   })
 
   let prev_t = performance.now();
   const loop = (t) => {
     state.step();
     const bodies = state.to_json();
-
-    let fontSize = 50;
-    let textIndex = 0;
-
-    // ctx.clearRect(0, 0, canvas.width, fontSize * 5);
-    ctx.clearRect(0, 0, canvas.width, canvas.height);
 
     for (const body of bodies) {
       ctx.fillStyle = 'red';
@@ -71,20 +75,24 @@ async function run() {
       ctx.stroke();
     }
 
-    ctx.fillStyle = 'black';
-    ctx.font = `${50}px serif`;
-    ctx.fillText(`FPS: ${state.latency_secs()}`, 0, (++textIndex * fontSize));
-    ctx.fillText(`FRAME: ${state.current_frame()}`, 0, (++textIndex * fontSize));
-    ctx.fillText(`TARGET: ${state.target_frame()}`, 0, (++textIndex * fontSize));
-    ctx.fillText(`PKT LOSS: ${state.packet_loss()}`, 0, (++textIndex * fontSize));
-    ctx.fillText(`BODIES: ${bodies.length}`, 0, (++textIndex * fontSize));
+    let fontSize = 50;
+    let textIndex = 0;
+    overlay_ctx.clearRect(0, 0, canvas.width, canvas.height);
+    overlay_ctx.fillStyle = 'black';
+    overlay_ctx.font = `${50}px serif`;
+
+    overlay_ctx.fillText(`FPS: ${state.latency_secs()}`, 0, (++textIndex * fontSize));
+    overlay_ctx.fillText(`FRAME: ${state.current_frame()}`, 0, (++textIndex * fontSize));
+    overlay_ctx.fillText(`TARGET: ${state.target_frame()}`, 0, (++textIndex * fontSize));
+    overlay_ctx.fillText(`PKT LOSS: ${state.packet_loss()}`, 0, (++textIndex * fontSize));
+    overlay_ctx.fillText(`BODIES: ${bodies.length}`, 0, (++textIndex * fontSize));
 
     if (mouseDown) {
-      ctx.strokeStyle = 'blue';
-      ctx.beginPath();
-      ctx.moveTo(mouseDown[0], mouseDown[1]);
-      ctx.lineTo(mousePos[0], mousePos[1]);
-      ctx.stroke();
+      overlay_ctx.strokeStyle = 'blue';
+      overlay_ctx.beginPath();
+      overlay_ctx.moveTo(mouseDown[0], mouseDown[1]);
+      overlay_ctx.lineTo(mousePos[0], mousePos[1]);
+      overlay_ctx.stroke();
     }
 
     requestAnimationFrame(loop);
