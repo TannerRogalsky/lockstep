f06986c880b79cf8809765cc493b38c0980d8754
Tanner Rogalsky <tanner@tannerrogalsky.com>
2020-10-07 11:26:11 -0400

    gen proto disk for benchmarks

--- nbody/benches/simulation.rs
+++ nbody/benches/simulation.rs
@@ -1,16 +1,33 @@
 use criterion::{criterion_group, criterion_main, BatchSize, Criterion};
 use nbody::*;
 
+fn proto_disk<R: rand::Rng>(
+    sim: &mut Simulation,
+    rng: &mut R,
+    count: usize,
+    (origin_x, origin_y): (f32, f32),
+    radius: f32,
+) {
+    for _ in 0..count {
+        let rand = rng.gen::<f32>() * 2. * std::f32::consts::PI;
+        let rand2 = rng.gen::<f32>();
+        let x = (radius * rand2) * rand.cos();
+        let y = (radius * rand2) * rand.sin();
+        let mag = (x * x + y * y).sqrt();
+
+        let mut body = Body::new_lossy(origin_x + x, origin_y + y, 1000.);
+        body.velocity.x = Float::from_num(y * (mag / 7000.));
+        body.velocity.y = Float::from_num(-x * (mag / 7000.));
+        sim.add_body(body);
+    }
+}
+
 pub fn criterion_benchmark(c: &mut Criterion) {
     let mut sim = Simulation::new();
-    sim.add_body(Body::new_lossy(0., 0., 10000.));
-    sim.add_body({
-        let mut body = Body::new_lossy(0., -200., 10.);
-        body.velocity.x = Float::from_num(3);
-        body
-    });
+    let mut rng = rand_pcg::Pcg64Mcg::new(0);
+    proto_disk(&mut sim, &mut rng, 1000, (0., 0.), 400.);
 
-    c.bench_function("sim 2", move |b| {
+    c.bench_function("sim proto disk", move |b| {
         b.iter_batched(|| sim.clone(), |mut sim| sim.step(), BatchSize::SmallInput)
     });
 }
